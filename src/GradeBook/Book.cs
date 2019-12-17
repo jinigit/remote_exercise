1d729d687774eb3e108f82da034f136a25d2366b
Su Jin Lee <slee@sgi.sk.ca>
2019-12-16 21:03:01 -0600

    Testcode added

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -6,7 +6,7 @@ namespace GradeBook{
     public class Book{
 
         private List<double> grades;
-        private string name;
+        public string Name;
         public void AddGrade(double grade){
             grades.Add(grade);
         }
@@ -35,7 +35,7 @@ namespace GradeBook{
 
         public Book(string name){
             grades = new List<double>();
-            this.name = name;
+            Name = name;
         }
     }
 
